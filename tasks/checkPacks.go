476b5dea6d9b1bfe80a3962b39d94d587841b8c2
AlexPresso <blond.alexandre@hotmail.com>
2021-11-07 20:37:52 +0100

    Add item_removed and pack_removed Clean checkSeason Move checkSeason in 20 min scheduled task

--- tasks/checkPacks.go
+++ tasks/checkPacks.go
@@ -14,18 +14,23 @@ func checkPacks(db *gorm.DB, embeds *[]discord.Embed) (packs []structures.Pack) 
 		utils.Log("An error occurred while fetching packs: " + err.Error())
 		return
 	}
-
-	var storedPacks []structures.Pack
-	db.Find(&storedPacks)
 	packsMap := make(map[string]*structures.Pack)
-	for _, pack := range storedPacks {
+	for _, pack := range packs {
 		pack := pack
 		packsMap[pack.PackID] = &pack
 	}
 
+	var dbPacks []structures.Pack
+	db.Find(&dbPacks)
+	dbPacksMap := make(map[string]*structures.Pack)
+	for _, pack := range dbPacks {
+		pack := pack
+		dbPacksMap[pack.PackID] = &pack
+	}
+
 	for i := 0; i < len(packs); i++ {
 		pack := &packs[i]
-		dbPack := packsMap[pack.PackID]
+		dbPack := dbPacksMap[pack.PackID]
 
 		if dbPack != nil {
 			pack.ID = dbPack.ID
@@ -33,11 +38,20 @@ func checkPacks(db *gorm.DB, embeds *[]discord.Embed) (packs []structures.Pack) 
 			if utils.AreDifferent(*dbPack, *pack) {
 				*embeds = append(*embeds, *services.MakeEmbed("pack_changed", *dbPack, *pack))
 			}
-		} else if len(packsMap) > 0 {
+		} else if len(dbPacksMap) > 0 {
 			*embeds = append(*embeds, *services.MakeEmbed("new_pack", nil, *pack))
 		}
 	}
 
 	db.Save(&packs)
+
+	for _, pack := range dbPacks {
+		pack := pack
+		if packsMap[pack.PackID] == nil {
+			db.Delete(&pack)
+			*embeds = append(*embeds, *services.MakeEmbed("pack_removed", nil, pack))
+		}
+	}
+
 	return
 }
